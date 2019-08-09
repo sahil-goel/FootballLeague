36eff6945aab7fb4a19b9a4bd2435c70d7464d0d
Sahil Goel <sahgoel@publicisgroupe.net>
2019-08-09 18:28:31 +0530

    first commit

--- src/main/java/com/sapient/football/league/rest/ErrorHandlerController.java
+++ src/main/java/com/sapient/football/league/rest/ErrorHandlerController.java
@@ -1,8 +1,6 @@
 package com.sapient.football.league.rest;
 
-import static org.apache.commons.lang.BooleanUtils.toBoolean;
-import static org.apache.commons.lang.StringUtils.isNotBlank;
-
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
-import com.google.common.base.Preconditions;
-
 import springfox.documentation.annotations.ApiIgnore;
 
 @ApiIgnore
@@ -21,21 +17,26 @@ import springfox.documentation.annotations.ApiIgnore;
 @RequestMapping("/error")
 public class ErrorHandlerController implements ErrorController {
 
-    private final ErrorAttributes errorAttributes;
+	private final ErrorAttributes errorAttributes;
 
-    @Autowired
-    public ErrorHandlerController(ErrorAttributes errorAttributes) {
-        Preconditions.checkNotNull(errorAttributes, "ErrorAttributes must not be null");
-        this.errorAttributes = errorAttributes;
-    }
+	@Autowired
+	public ErrorHandlerController(ErrorAttributes errorAttributes) {
+		this.errorAttributes = errorAttributes;
+	}
 
-    @Override
-    public String getErrorPath() {
-        return "/error";
-    }
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
 
-    @RequestMapping
-    public Map<String, Object> error(WebRequest webRequest) {
-        return errorAttributes.getErrorAttributes(webRequest, true);
-    }
+	@RequestMapping
+	public Map<String, Object> error(WebRequest webRequest) {
+		return getErrorAttributes(webRequest);
+	}
+
+	private Map<String, Object> getErrorAttributes(WebRequest webRequest) {
+		Map<String, Object> errorMap = new HashMap<>();
+		errorMap.putAll(errorAttributes.getErrorAttributes(webRequest, false));
+		return errorMap;
+	}
 }
